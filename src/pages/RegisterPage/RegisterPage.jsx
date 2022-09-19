9aedae78217396241b4bf33875afd49d87e7f473
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-09-19 17:09:00 +0100

    Added error handling to register page

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,3 +1,4 @@
+import { Notify } from 'notiflix';
 import { useSignUpMutation } from '../../redux/auth/auth-slice';
 import { RegisterForm } from '../../components/RegisterForm/RegisterForm';
 
@@ -16,9 +17,22 @@ const RegisterPage = () => {
 
     try {
       const payload = await addUser(user);
-      console.log(payload);
-      // TODO: add error handling
+      if (payload.error) {
+        switch (payload.error.status) {
+          case 400:
+            Notify.failure("Can't create user or user exists");
+            break;
+
+          case 500:
+          default:
+            Notify.failure('Server is unavaliable');
+            break;
+        }
+      } else {
+        Notify.success('Success registration');
+      }
     } catch (error) {
+      Notify.failure('Unexpected error');
       console.log(error);
     }
   };
