ce1278aa2c32e79cc19a523263babe7a9e79a5a4
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-09-22 23:50:17 +0100

    Added basename to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -13,14 +13,14 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+    <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
           <Container>
             <App />
           </Container>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
